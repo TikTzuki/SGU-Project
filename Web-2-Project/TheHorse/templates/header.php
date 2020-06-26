713fad23d0f7bb429772347ecec36f3ee44b94a4
TikTzuki <tranphanthanhlong18@gmail.com>
2020-06-26 21:27:30 +0700

    26/6

---
+++ Web-2-Project/TheHorse/templates/header.php
@@ -0,0 +1,161 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Home-2 || James </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="shop-announce">
+        WE ARE OPEN AND DELIVERING TO YOUR DOOR | VIEW OUT SHIPPING & DELIVERY PAGE FOR UPDATED SHIPPPING TIMEFRAMES
+    </div>
+    <header class="header">
+        <div class="row shop-header">
+            <div class="col-4">
+                <div class="nav header-nav">
+                    <div class="nav-list">
+                        <div class="nav-item-dropdown">
+                            <div class="dropbtn">
+                                <a class="nav-link " href=""> ĐÈN CHÙM </a>
+                            </div>
+                            <div class="dropdown-content">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="nav-item-dropdown">
+                            <div class="dropbtn">
+                                <a class="nav-link " href=""> ĐÈN CHÙM </a>
+                            </div>
+                            <div class="dropdown-content">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-2">
+                                        <ul>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Phòng Khách</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Cổ Điển</a>
+                                            </li>
+                                            <li>
+                                                <a class="drop-link" href="#"> Đèn Chùm Đơn Giản</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="header-logo">
+                    <a class="link-logo" href="/">
+                        <img src="/media/thehorselogobefore.png" alt="The Horse">
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 d-flex justify-content-end header-left-comp">
+                <div class="header-search search-bar-toggle">
+                    <a class="fa w3-large fa-search " href="#"></a>
+                </div>
+                <div class="header-user">
+                    <a class="link-account fa w3-large fa-user-o" href="/account">
+                    </a>
+                </div>
+                <div class="header-cart">
+                    <span class="link-cart-inner">
+                            <a class="fa w3-large fa-shopping-cart link-cart" href="#"></a>
+                            <span class="link-cart-count">0</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <body>
+
+</html>
