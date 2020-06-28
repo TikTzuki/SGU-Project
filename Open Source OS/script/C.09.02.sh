5dfd64d6c4c9a1af6b39df057720d3c57e2ed4e8
TikTzuki <tranphanthanhlong18@gmail.com>
2020-06-28 21:09:38 +0700

    28/6

---
+++ Open Source OS/script/C.09.02.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Today is: `date`"
+echo "Hello $LOGNAME !!!"
+echo "Your current working directory: `pwd`"
+echo "Your home directory: $HOME"
+read -p "Please press enter to finish: "
+echo "Thank you very much!!"
