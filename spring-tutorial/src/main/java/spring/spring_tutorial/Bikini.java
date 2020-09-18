c6c591224a3f45fb27922832bedc45993d2bae22
TikTzuki <tranphanthanhlong18@gmail.com>
2020-09-19 00:49:08 +0700

    19/9

--- spring-tutorial/src/main/java/spring/spring_tutorial/Bikini.java
+++ spring-tutorial/src/main/java/spring/spring_tutorial/Bikini.java
@@ -1,8 +1,10 @@
 package spring.spring_tutorial;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Primary
 public class Bikini implements Outfit{
 	public void wear() {
 		System.out.println("Mac bikini");
--- spring-tutorial/src/main/java/spring/spring_tutorial/Nude.java
+++ spring-tutorial/src/main/java/spring/spring_tutorial/Nude.java
@@ -1,5 +1,6 @@
 package spring.spring_tutorial;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
